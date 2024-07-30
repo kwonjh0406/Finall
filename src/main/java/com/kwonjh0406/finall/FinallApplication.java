0f8bf70073760e04e780fa445c859db1b02274b0
kwonjh0406 <kwonjh0406@gmail.com>
2024-07-30 16:24:56 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/kwonjh0406/finall/FinallApplication.java
@@ -0,0 +1,13 @@
+package com.kwonjh0406.finall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwonjh0406/finall/FinallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwonjh0406.finall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
